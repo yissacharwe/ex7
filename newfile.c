b115b0efa5148ff6dcb6c9b51a3bac323bf3768c
Intelligate <lev.welt@gmail.com>
2023-01-19 17:44:07 +0200

    new file

---
+++ newfile.c
@@ -0,0 +1,12 @@
+// ---------------- include -----------------
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// ------------------ main ------------------
+int main(int argc, char *argv[])
+{  
+    
+
+    return 0;
+}
